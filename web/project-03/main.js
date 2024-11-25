cc532d45c6ec9c2fb09bdc2fbe122042938b52b0
edvannps <edvansabino@gmail.com>
2024-11-25 13:35:44 -0300

    Week 2: Section 4, Project 3

---
+++ web/project-03/main.js
@@ -0,0 +1,15 @@
+
+let firstName = prompt("Digite o seu primeiro nome: ");
+let lastName = prompt("Digite o seu sobrenome: ");
+let age = parseInt(prompt("Digite a sua idade: "));
+const age2 = 2;
+
+age += age2;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+
+/*console.log("Meu nome é " + firstName + " " + lastName + '.')
+console.log("Eu tenho " + age + " anos de idade.")*/
