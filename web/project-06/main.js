9ca8f91f897b40cf19b650087101cfeeb73be651
edvannps <edvansabino@gmail.com>
2024-11-25 17:14:13 -0300

    Week 2: Section 7, Project 6

---
+++ web/project-06/main.js
@@ -0,0 +1,10 @@
+
+let friends = ['Alan', 'Mark', 'Julia', 'Bella']
+console.log(friends);
+
+friends[1] = 'Ana'; // Replace
+friends[4] = 'Maverick'; // Add itens
+friends[5] = 1;
+
+console.log(friends);
+console.log(friends[1]);
